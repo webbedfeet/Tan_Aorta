a27d11ea000d88185b3e841615153d1b58b6320b
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-04-04 22:17:39 -0400

    Fixed cleanData

--- DataMunging.R
+++ DataMunging.R
@@ -1,17 +1,34 @@
 ProjTemplate::reload()
 dat <- readRDS('data/rda/raw_data.rds')
 
-d <- dat %>% filter(sheet=='T5T6')
+d <- dat %>% filter(sheet == 'T5T6')
 
-angle <- d %>% filter(row >= 4, col == 1) %>% pull(numeric)
-name <- d %>% filter(row < 4, col >= 3) %>%
-  mutate(character = ifelse(is.na(character), as.character(numeric), character)) %>%
-  select(row, col, character) %>%
-  spread(row, character) %>%
-  unite(ID, `1`:`3`, sep='_') %>%
-  pull(ID)
-data <- d %>% filter(row >= 4, col >= 3) %>%
-  select(row, col, numeric) %>%
-  spread(col, numeric) %>%
-  select(-row) %>%
-  set_names(name)
+cleanData <- function(d) {
+  angle <- d %>% filter(row >= 4, col == 1) %>% pull(numeric)
+  name <- d %>% filter(row %in% c(1,3), col >= 3) %>%
+    mutate(character = ifelse(is.na(character), as.character(numeric),
+                              character)) %>%
+    select(row, col, character) %>%
+    filter(!is.na(character)) %>%
+    spread(row, character) %>%
+    unite(ID, `1`:`3`, sep = '_') %>%
+    pull(ID)
+  data <- d %>% filter(row >= 4, col >= 3) %>%
+    filter(!is.na(numeric)) %>%
+    select(row, col, numeric) %>%
+    spread(col, numeric) %>%
+    select(-row) %>%
+    set_names(name) %>%
+    cbind(angle) %>%
+    gather(variable, value, -angle) %>%
+    separate(variable, c('ID','Measure'), sep = '_') %>%
+    spread(Measure, value) %>%
+    select(Spine, ID, angle, Dist2Aorta, SyndHeight) %>%
+    arrange(ID, angle)
+  return(data)
+}
+
+dat <- tidyxl::xlsx_cells(dir(datadir, full.names = T))
+
+dat %>% nest(-sheet) %>%
+  mutate(cleaned = map(data, cleanData)) -> blah
