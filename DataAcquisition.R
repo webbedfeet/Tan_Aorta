1cbde204813fd6bac6d83e30b181dd299676e1f4
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2018-04-10 12:51:10 -0400

    Fixed path names

--- DataAcquisition.R
+++ DataAcquisition.R
@@ -3,5 +3,5 @@ datadir <- file.path(ProjTemplate::find_dropbox(), 'NIAMS','Ward','Tan_Aorta')
 dir.exists(datadir)
 # sheet_names <- openxlsx::getSheetNames(dir(datadir, full.names = T))
 
-dat <- tidyxl::xlsx_cells(dir(datadir, full.names = T))
+dat <- tidyxl::xlsx_cells(file.path(dirname(dir(datadir, full.names = T)), basename(dir(datadir))))
 saveRDS(dat, 'data/rda/raw_data.rds', compress=T)
