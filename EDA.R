2957aaf5671908f417097637aa77eb74ab0267fe
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-04-13 14:15:52 -0400

    Overall relationships

--- EDA.R
+++ EDA.R
@@ -8,15 +8,28 @@ ProjTemplate::reload()
 
 cleaned_dat <- readRDS('data/rda/cleaned_data.rds')
 
-ggplot(cleaned_dat, aes(Dist2Aorta, SyndHeight))+
-  geom_point(alpha = 0.1, size = 0.5)+
-  facet_wrap(~Spine)
+cleaned_dat %>% group_by(Spine) %>% summarize(NumberOfPts = length(unique(ID)))
+# Overall association with height and distance ------------------------------------------------
 
-ggplot(cleaned_dat %>% filter(Spine=='T5T6', ID=='10'), aes(angle, Dist2Aorta, color=SyndHeight))+
-  geom_point()
+ggplot(cleaned_dat, aes(Dist2Aorta, SyndHeight))+
+  geom_point(alpha=.1, size=0.5) + geom_smooth()
+
+
+ggplot(cleaned_dat, aes(Dist2Aorta, SyndHeight))+
+  geom_point(alpha=0.1, size=0.5)+
+  geom_smooth(aes(color=Spine), se=F)
+
+ggplot(cleaned_dat, aes(angle, Dist2Aorta))+
+  geom_point(alpha=0.1)+geom_smooth(aes(color = Spine))
+
+ggplot(cleaned_dat, aes(angle, SyndHeight))+
+  geom_point(alpha = 0.1, size = 0.5) +
+  geom_smooth(aes(color=Spine))
+
+ggplot(cleaned_dat %>% filter(Spine=='T5T6'), aes(Dist2Aorta, SyndHeight))+geom_point() +
+  facet_wrap(~ID)
+
 
 cleaned_dat %>% group_by(ID, Spine) %>%
-  mutate(HeightRank = rank(SyndHeight, ties.method='min')) %>%
-  top_n(-5,Dist2Aorta) %>%
-  mutate(meanHeightRank = mean(HeightRank)) %>%
-  arrange(ID, Spine, Dist2Aorta)-> blah
+  mutate(height_rank = rank(SyndHeight, ties.method = 'min')) %>%
+  top_n(-5, Dist2Aorta) %>% ungroup() %>% count(Spine, angle)
