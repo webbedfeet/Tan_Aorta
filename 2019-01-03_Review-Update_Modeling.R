1d599e4eaef1f2f35189385073b78222979c118e
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2019-01-03 15:22:40 -0500

    New file looking at analysis from paper reviews

---
+++ 2019-01-03_Review-Update_Modeling.R
@@ -0,0 +1,11 @@
+#' The paper reviews promoted an idea of modeling with the "complete cohort". We are
+#' interpreting that to mean that we want to provide overall p-values showing that the effect
+#' of the aorta on syndesmophyte formation (by showing that overall the nadir sector has
+#' the lowest height of syndesmophytes) exists, rather than being uniform.
+#'
+#' A couple of ideas will be tried.
+#'
+#' 1. Show an upward trend in syndesmophyte heights moving away from the nadir sector, and
+#'    that any other ordering of the sectors would not show a generally increasing pattern
+#' 2. Model the avg heights and/or the presence of syndesmophytes on location using a
+#'    repeated mea
